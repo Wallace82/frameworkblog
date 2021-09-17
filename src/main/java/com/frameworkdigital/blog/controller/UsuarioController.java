10df885e0d515aa529b5d8dd66e148d6d8c2cd9e
wallace.silva <wallace.silva@PM03600219332_.pmdf.local>
2021-09-17 08:25:16 -0300

    15 - Habilitando Cors no metodo  ADD usuário para este no angular

--- src/main/java/com/frameworkdigital/blog/controller/UsuarioController.java
+++ src/main/java/com/frameworkdigital/blog/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,7 +55,7 @@ public class UsuarioController {
 		
 	}
 	
-	
+	@CrossOrigin(maxAge = 1800, origins = {"/localhot:4200"})
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<?> adicionar(@RequestBody @Valid UsuarioDTO usuarioDTO) {
