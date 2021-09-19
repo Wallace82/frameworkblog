fc06e4b56d68ebc6b6d3387a96d8a927fef21bd1
gomesw@gmail.com <gomesw@gmail.com>
2021-09-18 21:47:47 -0300

    28 - Autenticaçao com oauth implementada

--- src/main/java/com/frameworkdigital/blog/config/SwaggerConfig.java
+++ src/main/java/com/frameworkdigital/blog/config/SwaggerConfig.java
@@ -1,14 +1,24 @@
 package com.frameworkdigital.blog.config;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
+import io.swagger.models.auth.In;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
@@ -18,13 +28,13 @@ public class SwaggerConfig {
    public Docket apiDocket() {
 
        Docket docket =  new Docket(DocumentationType.SWAGGER_2)
-                //.ignoredParameterTypes(AuthenticationPrincipal.class)
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.frameworkdigital.blog.controller"))
                 .paths(PathSelectors.any())
-               .build().apiInfo(metaData());
-//                .securitySchemes(Arrays.asList(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
-//                .securityContexts(Arrays.asList(securityContext()));
+               .build().apiInfo(metaData())
+                .securitySchemes(Arrays.asList(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
+                .securityContexts(Arrays.asList(securityContext()));
 
        return docket;
 
@@ -39,19 +49,19 @@ public class SwaggerConfig {
 	   .build();
 	   }
    
-//   private SecurityContext securityContext() {
-//	    return SecurityContext.builder()
-//	        .securityReferences(defaultAuth())
-//	        .forPaths(PathSelectors.ant("/rest/**"))
-//	        .build();
-//	}
-//
-//	List<SecurityReference> defaultAuth() {
-//	    AuthorizationScope authorizationScope
-//	        = new AuthorizationScope("ADMIN", "accessEverything");
-//	    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-//	    authorizationScopes[0] = authorizationScope;
-//	    return Arrays.asList(
-//	        new SecurityReference("Token Access", authorizationScopes));
-//	}
+   private SecurityContext securityContext() {
+	    return SecurityContext.builder()
+	        .securityReferences(defaultAuth())
+	        .forPaths(PathSelectors.ant("/*/**"))
+	        .build();
+	}
+
+	List<SecurityReference> defaultAuth() {
+	    AuthorizationScope authorizationScope
+	        = new AuthorizationScope("ADMIN", "accessEverything");
+	    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+	    authorizationScopes[0] = authorizationScope;
+	    return Arrays.asList(
+	        new SecurityReference("Token Access", authorizationScopes));
+	}
 }
